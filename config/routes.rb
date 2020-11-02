de0e3a22dcc0c68fa797010ba6da0bc59537c440
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-11-02 08:47:01 +0000

    [Start] 2-6

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#index'
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
