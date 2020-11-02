0b0526e7038d4331791633ed80fbe0f2b5947e7e
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-11-02 10:02:59 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2ab10ff82cc08857726cd5620a98304264b0c7be5c950fcf72f6c1383f3c4c852c1888d2d48ea780cc2df8ea90fa47120cbca5fcf8d18b42d338529392a688a6'
